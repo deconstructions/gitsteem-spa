39615f8430c6f0c8c0e3a9e994dd5b14da0a1a77
Robin <cartoux.robin@gmail.com>
2018-11-10 14:52:02 +0100

    Steemconnect login

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import * as steemconnect from 'steemconnect';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+    export class HomeComponent implements OnInit{
+
+    public loginUrl = "";
+    
+    public isAuth = false;
+
+    public expiresIn;
+
+    private api;
+
+    private user;
+
+    private metadata;
+
+    constructor(@Inject('STEEM_API') api: steemconnect, private route: ActivatedRoute) {
+        this.api = api;
+        route.queryParams.subscribe((params) => {
+            console.log(params['access_token']);
+            if (!this.isAuth) {
+                this.expiresIn = params['expires_in'];
+
+                this.api.setAccessToken(params['access_token']);
+                this.api.me(function (err, result)
+                {
+                    console.log(result);
+                    console.log(err);
+                  if (!err)
+                  {
+                      _this.user = result.account;
+                      console.log(this.user)
+                      _this.isAuth = true;
+                      _this.metadata = JSON.stringify(result.user_metadata, null, 2);
+                  }
+                });
+            }
+        });
+    }
+
+    ngOnInit()
+    {
+        this.loginUrl = this.api.getLoginURL();
+    }
 }
--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -3,13 +3,23 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as steemconnect from 'steemconnect';
 
 export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
 }
 
+export function initializeSteem() {
+  return steemconnect.Initialize({
+          app: 'gitsteem-co',
+          callbackURL: 'http://localhost:5000/',
+          scope: ['vote', 'comment']
+        });
+}
+
 const providers = [
-  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
+     { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] },
+     { provide: 'STEEM_API', useFactory: initializeSteem, deps: [] }
 ];
 
 if (environment.production) {
@@ -18,3 +28,4 @@ if (environment.production) {
 
 platformBrowserDynamic(providers).bootstrapModule(AppModule)
   .catch(err => console.log(err));
+   
