3003a3b8f37f005b1543aebfef4d158e34effa82
Robin <cartoux.robin@gmail.com>
2018-11-10 15:44:44 +0100

    Steem Log out

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
 import * as steemconnect from 'steemconnect';
+import * as steem from 'steem';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,11 +10,21 @@ import { ActivatedRoute } from '@angular/router';
 })
     export class HomeComponent implements OnInit{
 
-    public loginUrl = "";
+    public loginUrl : string = "";
     
-    public isAuth = false;
+    public isAuth : boolean = false;
+
+    public loading : boolean = false;
 
     public expiresIn;
+
+    public parentAuthor : string;
+
+    public parentPermlink : string;
+
+    public comments: Observable<any[]>;
+
+    public message;
 
     private api;
 
@@ -28,16 +40,15 @@ import { ActivatedRoute } from '@angular/router';
                 this.expiresIn = params['expires_in'];
 
                 this.api.setAccessToken(params['access_token']);
-                this.api.me(function (err, result)
+                this.api.me( (err, result) => 
                 {
                     console.log(result);
                     console.log(err);
                   if (!err)
                   {
-                      _this.user = result.account;
-                      console.log(this.user)
-                      _this.isAuth = true;
-                      _this.metadata = JSON.stringify(result.user_metadata, null, 2);
+                      this.user = result.account;
+                      this.isAuth = true;
+                      this.metadata = JSON.stringify(result.user_metadata, null, 2);
                   }
                 });
             }
@@ -48,4 +59,63 @@ import { ActivatedRoute } from '@angular/router';
     {
         this.loginUrl = this.api.getLoginURL();
     }
+
+    public loadComments() {
+      this.api.getContentReplies(this.parentAuthor, this.parentPermlink, (err, result) => {
+        if (!err) {
+          this.comments = result.slice(-5);
+        }
+      });
+    };
+
+    public comment() {
+      this.loading = true;
+      var permlink = steem.formatter.commentPermlink(this.parentAuthor, this.parentPermlink);
+      this.api.comment(this.parentAuthor, this.parentPermlink, this.user.name, permlink, '', this.message, '', (err, result) => {
+        console.log(err, result);
+        this.message = '';
+        this.loading = false;
+        this.loadComments();
+      });
+    };
+
+    public vote(author, permlink, weight) {
+      this.api.vote(this.user.name, author, permlink, weight, (err, result) => {
+        if (!err) {
+          alert('You successfully voted for @' + author + '/' + permlink);
+          console.log('You successfully voted for @' + author + '/' + permlink, err, result);
+          this.loadComments();
+        } else {
+          console.log(err);
+        }
+      });
+    };
+
+    public updateUserMetadata(metadata) {
+      this.api.updateUserMetadata(metadata, (err, result) => {
+        if (!err) {
+          alert('You successfully updated user_metadata');
+          console.log('You successfully updated user_metadata', result);
+          if (!err) {
+            this.user = result.account;
+            this.metadata = JSON.stringify(result.user_metadata, null, 2);
+          }
+        } else {
+          console.log(err);
+        }
+      });
+    };
+
+    public logout(){
+        this.api.revokeToken( (err, result) => {
+
+            if (err) {
+                console.log(err);
+            }
+            else {
+                 console.log('You successfully logged out', err, result);
+                  this.user = null;
+            }
+      });
+    };
 }
