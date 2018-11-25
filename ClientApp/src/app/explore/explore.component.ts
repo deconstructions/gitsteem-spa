a94dc995594a3a1afbf8794cfa7b3815646cbdf0
Robin <cartoux.robin@gmail.com>
2018-11-25 16:05:19 +0100

    Vote issue working. (number of net votes not updated, still possible to vote an already voted post)

--- ClientApp/src/app/explore/explore.component.ts
+++ ClientApp/src/app/explore/explore.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component, Inject, OnInit } from '@angular/core';
 import { IssuesService } from '../services/index';
+import * as steemconnect from 'steemconnect';
 
 @Component({
   selector: 'app-explore',
@@ -9,18 +10,27 @@ export class ExploreComponent implements OnInit{
 
     private loading: boolean;
     private issues: Issue[];
+    private api;
+    private user: string;
 
     constructor(
-        private issuesService: IssuesService) {
-
+        private issuesService: IssuesService,
+        @Inject('STEEM_API') api: steemconnect) {
+        this.api = api;
+       
     }
 
      ngOnInit() {
         this.loading = false;
+        this.user = localStorage.getItem('currentUser');
         this.loadIssues();
      }
 
     loadIssues() {
+        if (this.loading) {
+            return;
+        }
+
         this.loading = true;
         this.issuesService.fetchAllReposWithIssues(
             (repos) =>
@@ -41,4 +51,16 @@ export class ExploreComponent implements OnInit{
                 this.loading = false;
             });
     }
+
+    public vote(author, permlink, weight) {
+      this.api.vote(this.user, author, permlink, weight, (err, result) => {
+        if (!err) {
+          alert('You successfully voted for @' + author + '/' + permlink);
+          console.log('You successfully voted for @' + author + '/' + permlink, err, result);
+          this.loadIssues();
+        } else {
+          console.log(err);
+        }
+      });
+    };
 }
